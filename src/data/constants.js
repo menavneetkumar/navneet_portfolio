40a183f0277599bcb72f33f919f55931552b0b93
Navneet Kumar <menavneetkj@gmail.com>
2023-12-02 18:35:35 +0530

    final

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/menavneetkumar",
   resume:
-    "https://drive.google.com/file/d/1kRVMvfUNSwkI9zyZTPZtpNwkiNt7UvR0/view?usp=sharing",
+    "https://drive.google.com/file/d/1b8gimxWUWKeQx4vz5A-IONg2NGzaNkTF/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/navneet31",
   twitter: "https://twitter.com/Navneet423",
   insta: "#",
